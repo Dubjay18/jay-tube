23ecf499894cdcb9c838493807cf6e23dd97699a
Dubjay18 <jejeniyi7@gmail.com>
2023-07-07 21:15:06 +0100

    feat:set up react query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,26 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import "@/styles/global.css";
 import type { AppProps } from "next/app";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "react-query";
+
+const queryClient = new QueryClient();
 
 export default function App({
   Component,
   pageProps,
 }: AppProps) {
   return (
-    <ThemeProvider
-      attribute='class'
-      defaultTheme='system'
-      enableSystem>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider
+        attribute='class'
+        defaultTheme='system'
+        enableSystem>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
