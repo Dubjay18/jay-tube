781e60eb556795da002e9daf16c9bb54d374c0c6
Dubjay18 <jejeniyi7@gmail.com>
2023-07-11 14:57:26 +0100

    fix:time tag

--- components/TimeTag.tsx
+++ components/TimeTag.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function TimeTag({ text }: { text: string | undefined }) {
   return (
-    <div className='bg-black opacity-80 px-4 py-2 text-sm flex items-center justify-center rounded-lg text-white z-20 absolute bottom-5 '>
+    <div className='bg-black opacity-80 px-3 py-2 text-xs flex items-center justify-center rounded-lg text-white z-20 absolute bottom-7 left-6 '>
       {text}
     </div>
   );
